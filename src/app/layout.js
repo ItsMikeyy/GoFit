e5da094516813f36dcd83aaf0d1ee15570ce2417
ItsMikeyy <michaelcarey14@outlook.com>
2025-01-14 22:56:57 -0700

    started auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SessionWrapper from "./components/SessionWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,6 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <SessionWrapper>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -25,5 +27,6 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
+    </SessionWrapper>
   );
 }
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,11 @@
+"use client";
+import {signIn} from "next-auth/react"
 export default function Login() {
     return (
       <>
         <div>
           <h1 className="text-4xl">Login</h1>
+          <button onClick={() => signIn("google")}>Login</button>
         </div>
       </>
     );
